8bd3a73a1552d3681edee19169229864f86f3efd
jialan <2629977697@qq.com>
2023-04-24 23:39:45 +0800

    Merge pull request #2 from jialan123/home
    4.24

--- 数据结构/顺序表/Book_Management_System.cpp
+++ 数据结构/顺序表/Book_Management_System.cpp
@@ -857,3 +857,32 @@ void BorrowReturnManger()//借还书模块
         getchar();
     }
 }
+
+
+int main() {
+    //显示主函数的界面;
+    ShowMainMeun();
+    int iItem;
+    scanf("%d", &iItem);
+    while (iItem) {
+        switch (iItem) {
+            case 1:
+                MangaeBook();//图书管理模块
+                break;
+            case 2:
+                MangaeReader();//读者管理模块
+                break;
+            case 3:
+
+                BorrowReturnManger();//借还书模块
+                break;
+            default:
+                printf("\t\t请输入正确的数字!\n\t程序将于3秒后跳转到主菜单");
+                Sleep(3000);
+        }
+        ShowMainMeun();
+        scanf("%d", &iItem);
+        getchar();
+    }
+    return 0;
+}
